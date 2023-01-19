f0e0199ec1f5534702c9999809f450a3e8a570a2
Hans Zizold <hzizold@hotmail.com>
2023-01-18 21:16:37 -0400

    Processing Data

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :update_posts_counter
+
+  def recent_comments
+    Comment.where(post: self).order(updated_at: :desc).first(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+end
