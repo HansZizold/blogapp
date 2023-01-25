e914a34f36a06d70ce51f80c6d3599e507071b27
Hans Zizold <hzizold@hotmail.com>
2023-01-25 15:59:39 -0400

    views
    Partially completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
+    @comments = Comment.all
+  end
 
   def show; end
 end
