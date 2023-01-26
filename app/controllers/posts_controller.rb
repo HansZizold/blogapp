dd16bcf60e965a211984925ea1c79959a40412a6
Hans Zizold <hzizold@hotmail.com>
2023-01-26 17:37:58 -0400

    Views Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,12 @@ class PostsController < ApplicationController
     @users = User.all
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id])
-    @comments = Comment.all
   end
 
-  def show; end
+  def show
+    @users = User.all
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @comments = Comment.where(post_id: params[:id])
+  end
 end
