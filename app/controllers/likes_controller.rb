5ed4cc52eb091ecabe744fad4d5729596f1c0c3f
Hans Zizold <hzizold@hotmail.com>
2023-01-28 10:40:01 -0400

    Forms Optimizations

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,13 @@
 class LikesController < ApplicationController
   def create
-    like = Like.new
-    author = current_user
-    like.author = author
+    like = Like.new(author_id: current_user.id, post_id: params[:post_id])
     post = Post.find(params[:post_id])
-    like.post = post
 
     if like.save
       flash[:success] = 'Like added successfully'
     else
       flash.now[:error] = 'Error: Like could not be added'
     end
-    redirect_to user_post_path(author, post)
+    redirect_to user_post_path(current_user, post)
   end
 end
