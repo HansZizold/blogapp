ced94ad8896f72920ec85041a656659435d3b714
Hans P. Zizold D <hzizold@hotmail.com>
2023-03-30 19:22:32 -0400

    Devise Confirmable Removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_192517) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_152132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,10 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_192517) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "role"
   end
 
